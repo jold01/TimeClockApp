098970743ddf40ce2858be392522a0217d09313e
Jack Old <32105984+jold01@users.noreply.github.com>
2022-10-12 16:38:11 -0500

    Generated Spring Microservice

---
+++ TimeClockApp/src/main/java/com/tca/TimeClockApp/TimeClockAppApplication.java
@@ -0,0 +1,13 @@
+package com.tca.TimeClockApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeClockAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeClockAppApplication.class, args);
+	}
+
+}
